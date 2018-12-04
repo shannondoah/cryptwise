fd97a88e5d7495a03a3f89fecdec17fe4950104e
Shannon <shannon.mcdonald46@gmail.com>
2018-12-03 21:49:46 -0800

    Add HD wallet provider and play around with testnet config

--- truffle.js
+++ truffle.js
@@ -11,6 +11,7 @@
  *     gasPrice: 10000000000,
  *   },
  */
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -20,6 +21,16 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY, 2),
+      network_id: 3,
+      gas: 7000000,
+      gasPrice: 21
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 4
     }
   }
 };
